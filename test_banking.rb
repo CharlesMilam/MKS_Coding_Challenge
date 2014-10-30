27690a36fa73cb284a953234786aebd19da9c4dd
Milam <charles.milam@gmail.com>
2014-10-30 05:38:18 -0500

    Modify to follow testing scenario from coding challenge webpage

--- test_banking.rb
+++ test_banking.rb
@@ -5,10 +5,15 @@ chase = Bank.new("JP Morgan Chase")
 wells_fargo = Bank.new("Wells Fargo")
 me = Person.new("Charles", 500)
 friend1 = Person.new("Russ", 1000)
-# chase.open_account(me)
-# chase.open_account(friend1)
-# wells_fargo.open_account(me)
-# wells_fargo.open_account(friend1)
+chase.open_account(me)
+chase.open_account(friend1)
+wells_fargo.open_account(me)
+wells_fargo.open_account(friend1)
+
+# Withdrawing and Depositing
+chase.deposit(me, 200)
+chase.deposit(friend1, 300)
+chase.withdraw(me, 50)
 
 
 
