c2d914f493c3ac32918654cbbbf353e5d5a9b8c2
Milam <charles.milam@gmail.com>
2014-10-30 01:03:23 -0500

    Add shell of the two required classes

---
+++ banking.rb
@@ -0,0 +1,25 @@
+# Simulates a simple banking environment
+
+# Can support multiple banks, and multiple users
+
+# Contains two classes:
+
+# Person class 
+# A new person should have a name set and should have a certain 
+# amount of cash that the person keeps with him/her.
+
+# Bank class 
+# With the bank, you should be able to: 
+# open a new account using the person's name withdraw money from a person's account 
+# deposit money into a person's account 
+# transfer money to another bank
+
+# Milam 103014
+
+class Person
+
+end
+
+class Bank
+
+end
